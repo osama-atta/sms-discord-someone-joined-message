742409309a3fc3e6545c2e1decdda6fc804446d6
osama-atta <osama.obeid@hotmail.com>
2022-06-30 19:32:58 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import sendingEmail
+import discord
+
+TOKEN = "OTg0MjQwNzY2NzI3NjQzMjM2.GtUhYY.bB4PSRYXJOKiRg0VCoblNYEq1JeVC9EpTVsMHE"
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    number = "4025172332"
+    provider = "Mint Mobile"
+    email_address = "automatetextdisco@gmail.com"
+    email_password = "tcpwkmewleohbdbd"
+    sender_credentials = (email_address, email_password)
+
+    if before.channel is None and after.channel is not None:
+        message = "{} has joined".format(member.name)
+        subject = "{} voice channel".format(after.channel.name)
+        sendingEmail.send_sms_via_email(number, message, provider, sender_credentials, subject)
+
+    # if before.channel is not None and after.channel is None:
+    #     message = "{} has left".format(member.name)
+    #     subject = "{} voice channel".format(before.channel.name)
+    #     sendingEmail.send_sms_via_email(number, message, provider, sender_credentials, subject)
+
+
+def main():
+    client.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
